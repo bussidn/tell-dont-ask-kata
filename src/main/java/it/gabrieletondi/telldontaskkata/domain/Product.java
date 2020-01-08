75ead90d4cef8b00d8b394e4df3a90c91171f88d
bussidn <bussidn@gmail.com>
2020-01-08 15:38:34 +0100

    create Product builder

--- src/main/java/it/gabrieletondi/telldontaskkata/domain/Product.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/domain/Product.java
@@ -11,6 +11,16 @@ public class Product {
     private BigDecimal price;
     private Category category;
 
+    public Product() {
+
+    }
+
+    public Product(String name, BigDecimal price, Category category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
     public String getName() {
         return name;
     }
@@ -31,14 +41,38 @@ public class Product {
         this.category = category;
     }
 
-    public BigDecimal unitaryTax() {
+    BigDecimal unitaryTax() {
         return getPrice()
                 .divide(valueOf(100), getPrice().scale() + 2, RoundingMode.UNNECESSARY)
                 .multiply(category.getTaxPercentage())
                 .setScale(2, HALF_UP);
     }
 
-    public BigDecimal unitaryTaxedAmount() {
+    BigDecimal unitaryTaxedAmount() {
         return price.add(unitaryTax()).setScale(2, HALF_UP);
+    }
+
+    public static class Builder {
+        private final String name;
+        private BigDecimal price;
+        private Category category;
+
+        public Builder(String name) {
+            this.name = name;
+        }
+
+        public Builder price(BigDecimal price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder category(Category category) {
+            this.category = category;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(name, price, category);
+        }
     }
 }
