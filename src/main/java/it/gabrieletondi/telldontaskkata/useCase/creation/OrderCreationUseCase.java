f818ffd0f949bd3f130a41e27f67081236ddaeb8
bussidn <bussidn@gmail.com>
2020-01-08 14:48:16 +0100

    create Command for order creation

--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/creation/OrderCreationUseCase.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/creation/OrderCreationUseCase.java
@@ -2,14 +2,12 @@ package it.gabrieletondi.telldontaskkata.useCase.creation;
 
 import it.gabrieletondi.telldontaskkata.domain.OrderItem;
 import it.gabrieletondi.telldontaskkata.domain.Product;
-import it.gabrieletondi.telldontaskkata.domain.order.Order;
 import it.gabrieletondi.telldontaskkata.repository.OrderRepository;
 import it.gabrieletondi.telldontaskkata.repository.ProductCatalog;
 import it.gabrieletondi.telldontaskkata.useCase.UnknownProductException;
 
 public class OrderCreationUseCase {
 
-    private static final String EURO = "EUR";
     private final OrderRepository orderRepository;
     private final ProductCatalog productCatalog;
 
@@ -19,30 +17,9 @@ public class OrderCreationUseCase {
     }
 
     public void run(SellItemsRequest request) {
-        Order order = Order.initializeOrderWith(EURO, request.orderId());
 
-        for (SellItemRequest itemRequest : request.getRequests()) {
-            order.add(orderItemFrom(itemRequest));
-        }
-
-        orderRepository.save(order);
-    }
-
-    private OrderItem orderItemFrom(SellItemRequest itemRequest) {
-        return orderItem(productFrom(itemRequest), quantityFrom(itemRequest));
-    }
-
-    private int quantityFrom(SellItemRequest itemRequest) {
-        return itemRequest.getQuantity();
-    }
-
-    private Product productFrom(SellItemRequest itemRequest) {
-        return productCatalog.findByName(itemRequest.getProductName())
-                .orElseThrow(UnknownProductException::new);
-    }
-
-    private OrderItem orderItem(Product product, int quantity) {
-        return new OrderItem(product, quantity);
+        request.toSellItemsCommand(orderRepository, productCatalog)
+                .run();
     }
 
 }
--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/creation/SellItemsRequest.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/creation/SellItemsRequest.java
@@ -1,10 +1,16 @@
 package it.gabrieletondi.telldontaskkata.useCase.creation;
 
-import it.gabrieletondi.telldontaskkata.domain.OrderStatus;
+import it.gabrieletondi.telldontaskkata.domain.OrderItem;
+import it.gabrieletondi.telldontaskkata.domain.Product;
+import it.gabrieletondi.telldontaskkata.domain.order.Order;
+import it.gabrieletondi.telldontaskkata.repository.OrderRepository;
+import it.gabrieletondi.telldontaskkata.repository.ProductCatalog;
+import it.gabrieletondi.telldontaskkata.useCase.UnknownProductException;
 
 import java.util.List;
 
 public class SellItemsRequest {
+
     private final int id;
     private List<SellItemRequest> requests;
 
@@ -20,7 +26,51 @@ public class SellItemsRequest {
         return requests;
     }
 
-    int orderId() {
-        return id;
+    SellItemsCommand toSellItemsCommand(OrderRepository orderRepository, ProductCatalog productCatalog) {
+        return new SellItemsCommand(id, requests, orderRepository, productCatalog);
+    }
+
+    class SellItemsCommand {
+
+        private static final String EURO = "EUR";
+
+        private final int id;
+        private final List<SellItemRequest> requests;
+        private final OrderRepository orderRepository;
+        private final ProductCatalog productCatalog;
+
+        SellItemsCommand(int id, List<SellItemRequest> requests, OrderRepository orderRepository, ProductCatalog productCatalog) {
+            this.id = id;
+            this.requests = requests;
+            this.orderRepository = orderRepository;
+            this.productCatalog = productCatalog;
+        }
+
+        public void run() {
+            Order order = Order.initializeOrderWith(EURO, id);
+
+            for (SellItemRequest itemRequest : requests) {
+                order.add(orderItemFrom(itemRequest));
+            }
+
+            orderRepository.save(order);
+        }
+
+        private OrderItem orderItemFrom(SellItemRequest itemRequest) {
+            return orderItem(productFrom(itemRequest), quantityFrom(itemRequest));
+        }
+
+        private int quantityFrom(SellItemRequest itemRequest) {
+            return itemRequest.getQuantity();
+        }
+
+        private Product productFrom(SellItemRequest itemRequest) {
+            return productCatalog.findByName(itemRequest.getProductName())
+                    .orElseThrow(UnknownProductException::new);
+        }
+
+        private OrderItem orderItem(Product product, int quantity) {
+            return new OrderItem(product, quantity);
+        }
     }
 }
